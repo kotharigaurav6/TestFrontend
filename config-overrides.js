5de8921a9da86bb5b7c1691c27298c299d8742bb
gaurav <kotharigaurav6@gmail.com>
2024-12-30 16:04:18 +0530

    changes

--- config-overrides.js
+++ config-overrides.js
@@ -1,36 +1,12 @@
-// const webpack = require('webpack');
-// module.exports = {
-//   webpack: function (config, env) {
-//     config.resolve.fallback = {
-//       ...config.resolve.fallback,
-//       path: require.resolve('path-browserify'),
-//     };
-//     return config;
-//   },
-// };
+const { override, addWebpackAlias } = require('customize-cra');
 
-
-// import webpack from 'webpack';
-
-// export default {
-//   webpack: (config, env) => {
-//     config.resolve.fallback = {
-//       ...config.resolve.fallback,
-//       path: require.resolve('path-browserify'),
-//     };
-//     return config;
-//   },
-// };
-
-
-// config-overrides.js
-const webpack = require('webpack');
-module.exports = function override(config, env) {
+module.exports = override(
+  (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
-      path: require.resolve('path-browserify'),  // Polyfill for Node's 'path' module
+      os: require.resolve('os-browserify/browser'),
+      path: require.resolve('path-browserify'),
     };
-  
     return config;
-  };
-  
+  }
+);
