ed8004ce28d4ca3e4b027b6c81769afb64729bb8
gaurav <kotharigaurav6@gmail.com>
2024-12-30 19:26:05 +0530

    changes

--- config-overrides.js
+++ config-overrides.js
@@ -8,6 +8,8 @@ module.exports = override(
       path: require.resolve('path-browserify'),
       crypto: require.resolve('crypto-browserify'),
       buffer: require.resolve('buffer/'),
+      stream: require.resolve('stream-browserify'),
+      
     };
     return config;
   }
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
       path: require.resolve('path-browserify'),
       crypto: require.resolve('crypto-browserify'),
       buffer: require.resolve('buffer/'),
+      stream: require.resolve('stream-browserify'),
     },
   },
 };
